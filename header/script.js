0fbc84af9cd8aa0df2b4ae38bd208c91b7d7fdf5
Adriana Raubach <raubach.adrianadw@gmail.com>
2025-01-08 16:56:59 -0300

    refactor(header): turning the header into a component

---
+++ header/script.js
@@ -0,0 +1,25 @@
+let listItems = document.querySelectorAll("nav ul li a");
+
+for(let i = 0; i < listItems.length; i++) {
+    listItems[i].addEventListener("click", function() { 
+        const itemActive = document.querySelector('.active')
+        if (itemActive) {
+            itemActive.classList.remove('active');
+        }
+        listItems[i].classList.add('active')
+    })
+}
+
+let navIcon = document.querySelectorAll("button img");
+navIcon[0].addEventListener("click", function(){
+    let nav = document.querySelectorAll("header ul");
+    let access = document.querySelectorAll("header div");
+    const navActive = document.querySelector('.nav-active')
+        if (navActive) {
+            nav[0].classList.remove('nav-active');
+            access[0].classList.remove('nav-active');
+        } else{
+            nav[0].classList.add('nav-active')
+            access[0].classList.add('nav-active')
+        }
+})
